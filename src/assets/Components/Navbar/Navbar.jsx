fb9119599ce608915b69e15824fded7314183d25
Alin <alinsridhar1@gmail.com>
2026-02-13 23:35:41 +0530

    contact page

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -2,6 +2,12 @@ import style from './Navbar.module.css'
 import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom'
 export default function Navbar() {
+    const hidesidebar=()=>{
+        document.querySelector(`.${style.sidenavbar}`).style.display='none';
+    }
+    const showsidebar=()=>{
+        document.querySelector(`.${style.sidenavbar}`).style.display='block';
+    }
     return (
         <>
            <nav className={style.navbar}>
@@ -11,6 +17,22 @@ export default function Navbar() {
             </div>
             <div>
                 <ul className={style.navlinks}>
+                    <li  className={style.liitem}><a href="">Home</a></li>
+                    <li  className={style.liitem}><Link to="/aboutus">About</Link></li>
+                    <li  className={style.liitem}><a href="">Events</a></li>
+                    <li  className={style.liitem}><Link to="/contact">Contact</Link></li>
+                    <svg onClick={showsidebar} width="32" height="32" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={style.menuIcon}>
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </ul>
+            </div>
+            <div className={style.sidenavbar}>
+                <ul className={style.sidenavlinks}>
+                    <li style={{'padding':'10px'}} onClick={hidesidebar}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={style.closeIcon}>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                        </svg>
+</li>
                     <li><a href="">Home</a></li>
                     <li><Link to="/aboutus">About</Link></li>
                     <li><a href="">Events</a></li>
--- src/assets/Pages/Contact/Contact.jsx
+++ src/assets/Pages/Contact/Contact.jsx
@@ -1,6 +1,10 @@
+import Navbar from "../../Components/Navbar/Navbar";
 import style from "./Contact.module.css";
+import Contactus from "../../Components/Contactus/Contactus";
 export default function Contact() {
 	return (
         <>
+        <Navbar/>
+        <Contactus/>
         </>
 );}
