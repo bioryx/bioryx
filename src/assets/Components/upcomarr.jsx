5d1b9d6db0210ad07975695e2f251e83ede77bff
bioryx <bioryx.official@gmail.com>
2026-02-22 09:35:52 +0530

    Update upcomarr.jsx

--- src/assets/Components/upcomarr.jsx
+++ src/assets/Components/upcomarr.jsx
@@ -1,11 +1,10 @@
 export const upcoming =[
     {
-        image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29uZmVyZW5jZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
-        title: "BIOLOGICAL INVASIONS. SO WHAT? ",
-        host: "Dr T. V. Sajeev, Chief Scientist, Kerala Forest Research Institute",
+        title: "LICHENS: NEITHER PLANT NOR FUNGUS… SO WHAT ARE THEY ?",
+        host: "Dr Stephen Sequiera Associate Professor & Research Guide, Maharajas College, Ernakulam",
         location: "Google meet",
-        date: "February 21, 2026",
+        date: "February 28, 2026",
         time: "7:30 PM - 8:30 PM",
-        reglink:"https://forms.gle/ANsFYV4ppDpmVJkQ6"
+        reglink:"https://chat.whatsapp.com/IDyoVAj9X0QFfMpI2DTfC8"
     }
 ]
