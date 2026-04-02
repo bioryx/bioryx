ab7ea059e9d42f1c86f14e47285c0b8c024e4f31
bioryx <bioryx.official@gmail.com>
2026-04-02 22:49:09 +0530

    Update Homebody.jsx
    error resolved home page not loading

--- src/assets/Components/Homebody/Homebody.jsx
+++ src/assets/Components/Homebody/Homebody.jsx
@@ -6,22 +6,22 @@ export default function Homebody() {
         <>
         <div className={style.biomain}>
             <div className={style.biocard}>
-                <motion.div className={style.biotext} initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1}}>
+                <div className={style.biotext} >
                     <h1>BIORYX</h1>
                     <p>Bioryx is a student-run community for biology students interested in all fields of biological science, from genetics and microbiology to ecology, botany, zoology, and conservation. It’s a shared space for academic discussions, study resources, field experiences, event updates, and science-based conversations.</p>
-                </motion.div>
+                </div>
                 <img src="https://www.fauna-flora.org/wp-content/uploads/2017/06/naturepl_00568429.jpg" alt="" />
             </div>
             <div className={style.biocard2}>
                 <img src="https://i.pinimg.com/1200x/f2/e9/1a/f2e91a50ad0252a5731ec44444fa3b62.jpg" alt="" />
-                <motion.div className={style.biotext2} initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1}}>
+                <div className={style.biotext2} >
                     <p>Built on curiosity and respect for life sciences, Bioryx connects students to learn together, exchange ideas, and promote scientific thinking and environmental awareness.</p>
                     <ul>
                         <li>learn collaboratively</li>
                         <li>think scientifically</li>
                         <li>grow as biologists</li>
                     </ul>
-                </motion.div>
+                </div>
             </div>
             <div className={style.biobot}>
                 <h1>Bioryx - Where biology meets purpose...</h1>
