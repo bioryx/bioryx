d7ff0c8ced85ac106cb1b90fe0e7856d268ad43e
Alin <alinsridhar1@gmail.com>
2026-04-03 12:37:32 +0530

    reavct tilt removed

--- src/assets/Components/Peoplecard/Peoplecard.jsx
+++ src/assets/Components/Peoplecard/Peoplecard.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Tilt } from 'react-tilt';
+import Tilt from 'react-parallax-tilt';
 import './Peoplecard.css';
-
-const defaultTiltOptions = {
-	reverse:        false,  // reverse the tilt direction
-	max:            10,     // max tilt rotation (degrees)
-	perspective:    1500,   // Transform perspective, the lower the more extreme the tilt gets.
-	scale:          1.05,   // 2 = 200%, 1.5 = 150%, etc..
-	speed:          1500,   // Speed of the enter/exit transition
-	transition:     true,   // Set a transition on enter/exit.
-	axis:           null,   // What axis should be disabled. Can be X or Y.
-	reset:          true,   // If the tilt effect has to be reset on exit.
-	easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
-}
 
 const Peoplecard = ({ name, designation, img, occupation, email, phoneno }) => {
   return (
-    <Tilt options={defaultTiltOptions} className="people-card-tilt">
+    <Tilt 
+      tiltReverse={false}
+      tiltMaxAngleX={10}
+      tiltMaxAngleY={10}
+      perspective={1500}
+      scale={1.05}
+      transitionSpeed={1500}
+      className="people-card-tilt"
+    >
       <div className="people-card">
         <div className="people-card-img-container">
           <img src={img} alt={name} className="people-card-img" />
