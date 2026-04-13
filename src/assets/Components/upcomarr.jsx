c3b0b91c47821c076782f46334245b9f9ee2fd4b
Alin <alinsridhar1@gmail.com>
2026-04-13 11:55:20 +0530

    events updated

--- src/assets/Components/upcomarr.jsx
+++ src/assets/Components/upcomarr.jsx
@@ -5,5 +5,12 @@ export const upcoming =[
         location: "Google Meet",
         date: "18 April 2026",
         time: "7:30 PM - 8:30 PM"
+    },
+    {
+        title: "Mind Control in Nature: Parasites That Manipulate Insects",
+        host: "Ashwati Biju - MSc Wildlife Science, Wildlife Institute of India (2024–26)",
+        location: "Google Meet",
+        date: "19 April 2026",
+        time: "7:30 PM - 8:30 PM"
     }
 ]
